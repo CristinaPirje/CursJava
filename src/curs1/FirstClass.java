cceb7efbf5c5a6f5fa8055f18f3fa7376744911f
unknown <pirje.cristina@gmail.com>
2024-03-04 11:40:43 +0200

    Curs1

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,17 @@
+package curs1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		FirstClass obiect = new FirstClass();
+				obiect.greetings();
+		
+	}
+public void greetings() {
+	
+	System.out.println("Salut");
+}
+	
+}
