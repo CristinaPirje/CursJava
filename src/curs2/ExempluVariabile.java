c1f5aa47e182371ddcad8d2b90e9395e56ce98c4
unknown <pirje.cristina@gmail.com>
2024-03-07 10:35:15 +0200

    curs2 intermediar

---
+++ src/curs2/ExempluVariabile.java
@@ -0,0 +1,22 @@
+package curs2;
+
+public class ExempluVariabile {
+	//variabila de instanta
+int varsta;
+String prenume;
+
+	public static void main(String[] args) {
+		
+		//declarare si initializare
+		String nume = "Bob";
+		//declarare variabila(var age are valoarea default pt data typeint)
+		int age;
+		ExempluVariabile obiect=new ExempluVariabile();
+		System.out.println(nume);
+		System.out.println(obiect.varsta);
+		System.out.println(obiect.prenume);
+		//System.out.println(obiect.prenume.length());
+
+	}
+
+}
